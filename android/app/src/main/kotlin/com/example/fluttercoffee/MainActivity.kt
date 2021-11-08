33ca5483ecc169d8ffabafd1ff696f41b33e1d62
alreadyknowit <demiriabdurrahman@gmail.com>
2021-11-08 18:27:10 +0300

    firs push

---
+++ android/app/src/main/kotlin/com/example/fluttercoffee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttercoffee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: const Text("Transform in Flutter"),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(25),
+              margin: EdgeInsets.all(22),
+              color: Colors.amber,
+              child: Transform(
+                child: Text('Example 1'),
+                transform: Matrix4.rotationX(0.2),
+                alignment: FractionalOffset.center,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(25),
+              margin: EdgeInsets.all(22),
+              color: Colors.amber,
+              child: Transform(
+                child: Text('Example 4'),
+                transform: Matrix4.rotationX(0.2),
+                alignment: FractionalOffset.center,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(25),
+              margin: EdgeInsets.all(22),
+              color: Colors.amber,
+              child: Transform(
+                child: Text('Example 3'),
+                transform: Matrix4.rotationX(0.2),
+                alignment: FractionalOffset.center,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(25),
+              margin: EdgeInsets.all(22),
+              color: Colors.amber,
+              child: Transform(
+                child: Text('Example 2'),
+                transform: Matrix4.rotationX(0.2),
+                alignment: FractionalOffset.center,
+              ),
+            ),
+
+          
+          ],
+        )),
+      ),
+    );
+  }
+}
